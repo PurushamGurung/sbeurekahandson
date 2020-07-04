c1161762ce1c661f58e29bcfc0b8fce09c1aaaf4
PurushamGurung <63781893+PurushamGurung@users.noreply.github.com>
2020-07-04 20:52:44 +0530

    Add files via upload

---
+++ Lambok/Apps.java
@@ -0,0 +1,12 @@
+package streamsss;
+
+public class Apps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LombokDemo obj=new LombokDemo(1,"@2","22");
+		
+		System.out.println(obj);
+	}
+
+}
---
+++ Lambok/LombokDemo.java
@@ -0,0 +1,12 @@
+package streamsss;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+@Data
+@AllArgsConstructor
+
+public class LombokDemo {
+	private int data;
+	private String name;
+	private String idk;
+}
